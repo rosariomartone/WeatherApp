9a0a9b86056de61e0bdd24c0653860799b8cf47a
Rosario Martone <rosario.martone@janes.com>
2022-01-11 21:18:10 +0000

    Adding some tests and code cleaning..

---
+++ WeatherAppFMW.Tests/ForecastRange_Tests.cs
@@ -0,0 +1,219 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using WeatherAppFMW.Helpers;
+using WeatherAppFMW.Models;
+
+namespace WeatherAppFMW.Tests
+{
+    [TestClass]
+    public class ForecastRange_Tests
+    {
+        private Forecastday forecasts = null;
+
+        [TestInitialize]
+        public void ForecastRange_Init()
+        {
+            forecasts = new Forecastday();
+            forecasts.Hour = new List<Hour>()
+            {
+                new Hour {
+                    Time = "2022-01-11 00:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 01:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 02:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 03:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 04:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 05:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 06:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 07:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 08:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 09:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 10:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 11:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 12:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 13:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 14:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 15:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 16:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 17:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 18:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 19:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 20:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 21:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Cloudy"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 22:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Clear"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 23:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Clear"
+                    }
+                },
+                new Hour {
+                    Time = "2022-01-11 24:00",
+                    Condition = new Condition()
+                    {
+                        Text = "Clear"
+                    }
+                }
+            };
+        }
+
+        [TestMethod]
+        [DataRow(3)]
+
+        public void GetForecastRange_Retrieve(int hour)
+        {
+            List<Hour> hours = ForecastRange.GetForecastRange(forecasts, hour);
+            Assert.IsTrue(hours.Count == 3);
+            Assert.IsTrue(hours[0].Time.Equals("2022-01-11 03:00"));
+            Assert.IsTrue(hours[1].Time.Equals("2022-01-11 04:00"));
+            Assert.IsTrue(hours[2].Time.Equals("2022-01-11 05:00"));
+        }
+
+        [TestMethod]
+        [DataRow(23)]
+
+        public void GetForecastRange_RetrieveOneHour(int hour)
+        {
+            List<Hour> hours = ForecastRange.GetForecastRange(forecasts, hour);
+            Assert.IsTrue(hours.Count == 1);
+            Assert.IsTrue(hours[0].Time.Equals("2022-01-11 23:00"));
+        }
+    }
+}
---
+++ WeatherAppFMW.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeatherAppFMW.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeatherAppFMW.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4aba82ae-e9f9-4f81-b426-e3c8ed0569a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
