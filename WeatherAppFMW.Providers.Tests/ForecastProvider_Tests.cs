184fea706eef6b9625d9f29b2f905aeaa26b7855
Rosario Martone <rosario.martone@janes.com>
2022-01-16 15:24:56 +0000

    Code cleaning.

--- WeatherAppFMW.Providers.Tests/ForecastProvider_Tests.cs
+++ WeatherAppFMW.Providers.Tests/ForecastProvider_Tests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WeatherAppFMW.Providers.Tests
 {
